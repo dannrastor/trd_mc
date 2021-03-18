9592c7af1dcfc98d7f6bbd4b9829c1d8de5652bf
Daniil Rastorguev <dannraptor@gmail.com>
2021-03-18 13:13:44 +0300

    constexpr constants

--- sophisticated/constants.h
+++ sophisticated/constants.h
@@ -1,25 +1,25 @@
 //detector parameters
-#define THR 4.2
-#define PAIR_ENERGY 0.0042
-#define NOISE 80 //electrons
-#define FANO 0.14
-#define PX_SIZE 55.0
-#define THICKNESS 500.0
+constexpr double THR = 4.2;
+constexpr double PAIR_ENERGY = 0.0042;
+constexpr double NOISE = 80; //electrons
+constexpr double FANO = 0.14;
+constexpr double PX_SIZE = 55.0;
+constexpr double THICKNESS = 500.0;
 
 
 //absorption parameters
 // ln(mu[cm^-1]) = A * ln(E[keV]) + B
 //for E < 10 keV (see NIST)
-#define A1 -2.658
-#define B1 11.401
+constexpr double A1 = -2.658;
+constexpr double B1 = 11.401;
 //for E > 10 keV
-#define A2 -2.735
-#define B2 13.570
+constexpr double A2 = -2.735;
+constexpr double B2 = 13.570;
 
 
 //fluo parameters
-#define FLUO_RATE 0.5
-#define FLUO_BRANCH 0.5
-#define FLUO_DISTANCE_GA 40.0
-#define FLUO_DISTANCE_AS 15.0
-#define FLUO_ENERGY 10.0
+constexpr double FLUO_RATE = 0.5;
+constexpr double FLUO_BRANCH = 0.5;
+constexpr double FLUO_DISTANCE_GA = 40.0;
+constexpr double FLUO_DISTANCE_AS = 15.0;
+constexpr double FLUO_ENERGY = 10.0;
