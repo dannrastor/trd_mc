df7f5f268c8ece186d94fcaaa387ad6265e31dcd
dannrastor <dannraptor@gmail.com>
2021-03-25 15:22:04 +0300

    added some stupid test fixed critical bug (length units in wp)

--- sophisticated/src/response.cpp
+++ sophisticated/src/response.cpp
@@ -3,6 +3,7 @@
 
 #include <algorithm>
 #include <cmath>
+#include <iostream>
 
 using namespace std;
 
@@ -37,27 +38,31 @@ double ResponseGenerator::GetPixelRelatedV(double x, double y, double z, int x_p
         }
     }
 
-    return wp.GetV(x_eff, y_eff, z);
+    return wp.GetV(x_eff*1e-6, y_eff*1e-6, z*1e-6);
 }
 
 map<pair<int, int>, double> ResponseGenerator::Process(const vector<PhotonHit>& hits) {
     map<pair<int, int>, double> result;
 
     //simulation parameters
-    int n_group_electrons = 10;
-    int n_steps = 10;
+    int n_group_electrons = 1;
+    int n_steps = 100;
 
     for (const auto& hit : hits) {
 
         //initial ionization and its fluctuations limited by the Fano factor
         double n_electrons = hit.energy / PAIR_ENERGY;
+
         n_electrons = Smear(n_electrons, sqrt(n_electrons)*FANO);
+
 
         int n_groups = n_electrons / n_group_electrons;
 
-        double drift_time = hit.z; //FIXME
-        double cloud_size = InitialCloudSize(hit.energy);
-        double diffusion_sigma = sqrt(DIFFUSION_COEFF * drift_time);
+        double drift_time = 1; //FIXME
+        double cloud_size = 0; //InitialCloudSize(hit.energy);
+        double diffusion_sigma = 0; //sqrt(DIFFUSION_COEFF * drift_time);
+
+
 
         for (int i_group = 0; i_group < n_groups; ++i_group) {
             double xi = Smear(hit.x, cloud_size);
@@ -68,7 +73,10 @@ map<pair<int, int>, double> ResponseGenerator::Process(const vector<PhotonHit>& 
             double yf = Smear(yi, diffusion_sigma);
             double zf = 0;
 
-            double charge = n_group_electrons;
+
+
+            double charge = n_electrons / n_groups;
+
 
             for (int i_step = 0; i_step < n_steps; ++i_step) {
                 double x_prestep = xi + (xf - xi) * i_step / n_steps;
@@ -83,11 +91,12 @@ map<pair<int, int>, double> ResponseGenerator::Process(const vector<PhotonHit>& 
                     for (int y_pixel = -1; y_pixel <= 1; ++y_pixel) {
                             double v_pre = GetPixelRelatedV(x_prestep, y_prestep, z_prestep, x_pixel, y_pixel);
                             double v_post = GetPixelRelatedV(x_poststep, y_poststep, z_poststep, x_pixel, y_pixel);
+                            //if (x_pixel * x_pixel + y_pixel*y_pixel == 0) cout << x_pixel << " " << y_pixel << " " << v_post << " " << v_pre << endl;
                             result[{x_pixel, y_pixel}] += charge * (v_post - v_pre);
                     }
                 }
 
-                charge *= exp(-drift_time / n_steps / LIFETIME);
+                //charge *= exp(-drift_time / n_steps / LIFETIME);
 
             }
 
